49bedf486ebf75be73d093c906f9215d008fe07e
Daniil <Phlash9@mail.ru>
2015-12-08 17:18:39 +0300

    "Init Commit"

--- DotaSniffer/main.c
+++ DotaSniffer/main.c
@@ -1,15 +1,5 @@
-//
-//  main.c
-//  DotaSniffer
-//
-//  Created by Daniil Popov on 08/12/2015.
-//  Copyright © 2015 None. All rights reserved.
-//
-
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
     return 0;
 }
